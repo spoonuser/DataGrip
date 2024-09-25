4ec3198c2161dced99b77d396618d06d5ca5ddb3
spoonuser <157282519+spoonuser@users.noreply.github.com>
2024-09-25 12:44:02 +0400

    Add files via upload

--- console.sql
+++ console.sql
@@ -1,21 +1,31 @@
-create database lab1
-create table usersss(
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50)
+DROP TABLE IF EXISTS countrie;
+DROP DATABASE IF EXISTS lab_2;
+CREATE DATABASE lab_2;
+
+CREATE TABLE countrie (
+    country_id SERIAL PRIMARY KEY,
+    country_name VARCHAR(255) DEFAULT 'Kazakhstan',
+    region_id INT,
+    population INT
 );
 
-alter table usersss
-add is_admin int default 1;
-alter table usersss
-alter column is_admin type boolean
-using is_admin::boolean;
-create table tasks_2(
-    id serial primary key,
-    name varchar(50),
-    user_id int
-);
-insert into usersss(first_name, last_name)
-values('Alpamys','Kaliyev');
-select * from usersss;
+INSERT INTO countrie (country_name, region_id, population) VALUES ('USA', 1, 331002651);
+INSERT INTO countrie (country_name) VALUES ('Canada');
+INSERT INTO countrie (country_name, region_id, population) VALUES ('Australia', 3, 25000000);
+INSERT INTO countrie (country_name, region_id, population) VALUES
+('India', 3, 1393409038),
+('China', 3, 1444216107),
+('Brazil', 2, 213993437);
 
+INSERT INTO countrie (region_id, population) VALUES (4, 18500000);
+
+CREATE TABLE countrie_new AS TABLE countrie WITH NO DATA;
+INSERT INTO countrie_new SELECT * FROM countrie;
+
+UPDATE countrie SET region_id = 1 WHERE region_id IS NULL;
+
+SELECT country_name, population * 1.1 AS "New Population" FROM countrie;
+DELETE FROM countrie WHERE population < 100000;
+
+DELETE FROM countrie_new WHERE country_id IN (SELECT country_id FROM countrie) RETURNING *;
+DELETE FROM countrie RETURNING *;
