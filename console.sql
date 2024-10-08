e438cfaa243c1c2e0e8858f81687daed4f494977
spoonuser <157282519+spoonuser@users.noreply.github.com>
2024-10-08 20:28:20 +0400

    Add files via upload

--- console.sql
+++ console.sql
@@ -1,8 +1,7 @@
 create database lab4;
-drop table Warehouses;
-drop table Boxes;
 CREATE TABLE Warehouses (
-    code INTEGER PRIMARY KEY,    location VARCHAR(255),
+    code INTEGER PRIMARY KEY,
+    location VARCHAR(255),
     capacity INTEGER
 );
 
@@ -19,7 +18,6 @@ CREATE TABLE Boxes (
     warehouse INTEGER,
     FOREIGN KEY (warehouse) REFERENCES Warehouses(code)
 );
-
 INSERT INTO Boxes (code, contents, value, warehouse) VALUES
 ('OMN7', 'Rocks', 180, 3),
 ('4HBP', 'Rocks', 250, 1),
@@ -30,14 +28,13 @@ INSERT INTO Boxes (code, contents, value, warehouse) VALUES
 ('9JF6', 'Papers', 175, 2),
 ('LL08', 'Rocks', 140, 4),
 ('P0H6', 'Scissors', 125, 1),
-('P2T6', 'Scissors', 150, 2),
-('TU55', 'Papers', 90, 5);
+('P2T6', 'Scissors', 150, 2)
+;
+
 SELECT * FROM Warehouses;
-SELECT * FROM Boxes
+SELECT  * FROM  Boxes
 WHERE value > 150;
-SELECT warehouse, COUNT(*) AS box_count
-FROM Boxes
-GROUP BY warehouse;
+SELECT DISTINCT contents from Boxes;
 SELECT warehouse, COUNT(*) AS box_count
 FROM Boxes
 GROUP BY warehouse;
@@ -46,19 +43,21 @@ FROM Boxes
 GROUP BY warehouse
 HAVING COUNT(*) > 2;
 INSERT INTO Warehouses (code, location, capacity)
-VALUES (6, 'New York', 3);
+VALUES (7, 'New York', 3);
 INSERT INTO Boxes (code, contents, value, warehouse)
 VALUES ('H5RT', 'Papers', 200, 2);
 UPDATE Boxes
-SET value = value * 0.85
-WHERE code = (
-    SELECT code
-    FROM (
-        SELECT code, ROW_NUMBER() OVER (ORDER BY value DESC) AS row_num
-        FROM Boxes
-    ) AS ranked
-    WHERE row_num = 3
-);
+set value = value*0.85
+where code = (
+    select code
+    from(
+        select code, row_number() over(order by value desc) as row_num
+        from Boxes
+
+        )
+    AS ranked
+    where row_num = 3
+    );
 DELETE FROM Boxes
 WHERE value < 150;
 DELETE FROM Boxes
@@ -68,4 +67,3 @@ WHERE warehouse IN (
     WHERE location = 'New York'
 )
 RETURNING *;
-
